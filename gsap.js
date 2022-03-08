4ab7ddc140b8ae2e0b94e084db1bfaad550349e6
Julia Rakityanska <96792225+JuliaRa411@users.noreply.github.com>
2022-03-08 16:31:41 -0500

    Add files via upload

---
+++ gsap.js
@@ -0,0 +1,25 @@
+gsap.to(".one",{y:300,
+ 
+     duration:3.5,
+    delay:.9
+
+})  
+
+
+
+gsap.to(".two",{y:100,
+ 
+    duration:3.5,
+   delay:.9
+
+})  
+
+
+gsap.from(".social",{y:100,
+ 
+duration:3.5,
+   delay:.9
+
+})  
+
+social
